6f6825891c4fefe523aab8a9e506b37909c9106f
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-06-23 19:24:19 +0300

    first commit

---
+++ задача 19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите пятизначное число: ");
+string? input= Console.ReadLine();
+string? input2 = input; 
+char[] input_array = input!.ToCharArray();
+Array.Reverse(input_array);
+input = new string(input_array);
+if (input == input2) {
+    System.Console.WriteLine("да");
+}
+else {
+    System.Console.WriteLine("нет");
+}
+
+
+
+
